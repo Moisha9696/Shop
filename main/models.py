5f3d8841dde573a7e3df148d4c5abda82cb7da7c
moisha <mikhail.yukhovich@gmail.com>
2025-11-01 19:39:57 +0300

    add cart

--- main/models.py
+++ main/models.py
@@ -44,6 +44,8 @@ class Product(models.Model):
     )
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=50, unique=True)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
 
     class Meta:
         ordering = ['name']
