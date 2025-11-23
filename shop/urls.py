4da13cef1d9baf45d610bf4514909f0be6ebca97
moisha <mikhail.yukhovich@gmail.com>
2025-11-23 18:30:25 +0300

    изменение поля для кол-ва товаров

--- shop/urls.py
+++ shop/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 
 
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +28,5 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
